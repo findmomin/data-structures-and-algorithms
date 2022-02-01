c6cc1feba87c5078aa31df70756355172f5e2130
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-02 04:40:09 +0600

    Added countUniqueValues

---
+++ challenges/countUniqueValues.js
@@ -0,0 +1,32 @@
+// A function that accepts a sorted array of numbers, and counts the
+// unique values in the array
+const countUniqueValues = (arr = []) => {
+  if (!arr.length) return 0;
+
+  let left = 0;
+  let right = 1;
+
+  while (right < arr.length) {
+    // if the numbers are not same
+    if (arr[left] !== arr[right]) {
+      // increment left
+      left++;
+
+      // replace left number with right
+      arr[left] = arr[right];
+    }
+
+    right++;
+  }
+
+  return left + 1;
+};
+
+// 2
+console.log(countUniqueValues([1, 1, 1, 1, 2]));
+
+// 0
+console.log(countUniqueValues([]));
+
+// 4
+console.log(countUniqueValues([-2, -1, -1, 0, 1]));
