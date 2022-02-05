87eeb642bcdd609b98afbe57ec13e03df609f01f
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-05 06:43:54 +0600

    Completed bubbleSort

--- sortingAlgorithms/bubbleSort.js
+++ sortingAlgorithms/bubbleSort.js
@@ -1,12 +1,15 @@
 /* 
-   A function that accepts 
+   A function that accepts an array of integers and returns the 
+   sorted array in ascending order. 
  */
-const bubbleSort = () => {
-  //
+const bubbleSort = (arr = []) => {
+  for (let i = arr.length - 1; i > -1; i--) {
+    for (let j = 0; j < i - 1; j++)
+      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+  }
+
+  return arr;
 };
 
-//
-console.log(bubbleSort());
-
-//
-console.log(bubbleSort());
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 21, 34, 44]
+console.log(bubbleSort([9, 1, 3, 44, 2, 5, 4, 21, 6, 34, 8, 7]));
