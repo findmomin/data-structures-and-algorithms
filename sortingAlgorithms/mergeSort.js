0a8fe9fd1b9e547cd845819ca0da1291f8e2313d
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-06 23:35:53 +0600

    Added mergeSort starter

---
+++ sortingAlgorithms/mergeSort.js
@@ -0,0 +1,10 @@
+/* 
+   A function that accepts an array of integers and returns the 
+   sorted array in ascending order. 
+ */
+const mergeSort = (arr = []) => {
+  //
+};
+
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 21, 34, 44]
+console.log(mergeSort([9, 1, 3, 44, 2, 5, 4, 21, 6, 34, 8, 7]));
