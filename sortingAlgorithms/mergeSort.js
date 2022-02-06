52bb89b854260825cc3182db362ceb7bfc2b25bb
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-07 05:21:44 +0600

    Added mergeSort

--- sortingAlgorithms/mergeSort.js
+++ sortingAlgorithms/mergeSort.js
@@ -2,9 +2,43 @@
    A function that accepts an array of integers and returns the 
    sorted array in ascending order. 
  */
-const mergeSort = (arr = []) => {
-  //
+const merge = (arr1 = [], arr2 = []) => {
+  let idxOne = 0;
+  let idxTwo = 0;
+  const results = [];
+
+  while (idxOne < arr1.length && idxTwo < arr2.length) {
+    if (arr1[idxOne] < arr2[idxTwo]) {
+      results.push(arr1[idxOne]);
+      idxOne++;
+    } else {
+      results.push(arr2[idxTwo]);
+      idxTwo++;
+    }
+  }
+
+  while (idxOne < arr1.length) {
+    results.push(arr1[idxOne]);
+    idxOne++;
+  }
+
+  while (idxTwo < arr2.length) {
+    results.push(arr2[idxTwo]);
+    idxTwo++;
+  }
+
+  return results;
 };
 
-// [1, 2, 3, 4, 5, 6, 7, 8, 9, 21, 34, 44]
-console.log(mergeSort([9, 1, 3, 44, 2, 5, 4, 21, 6, 34, 8, 7]));
+const mergeSort = (arr = []) => {
+  if (arr.length <= 1) return arr;
+
+  const midPoint = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, midPoint));
+  const right = mergeSort(arr.slice(midPoint));
+
+  return merge(left, right);
+};
+
+// [1, 2, 10, 14, 50, 99, 100]
+console.log(mergeSort([50, 99, 100, 1, 1, 10, 2, 14]));
