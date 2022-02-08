aad7a8aacff864c0bce0d506b98d6d2a2ceae6ea
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-09 05:22:37 +0600

    Added push to singlyLinkedList

--- data structures/singlyLinkedList.js
+++ data structures/singlyLinkedList.js
@@ -11,8 +11,30 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.length) this.head = this.tail = newNode;
+    else this.tail = this.tail.next = newNode;
+
+    this.length++;
+
+    return this;
+  }
 }
 
 const list = new SinglyLinkedList();
+
+// Push
+list.push('Hello');
+list.push('world');
+list.push('Hi');
+
+// Pop
+
+// Shift
+
+// Unshift
 
 console.log(list);
