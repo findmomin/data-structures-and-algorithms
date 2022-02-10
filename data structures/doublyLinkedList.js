431d7cd4c4f0aa90fb9985922c933eff6ab39603
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-10 15:05:39 +0600

    Added doublyLinkedList

---
+++ data structures/doublyLinkedList.js
@@ -0,0 +1,21 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
+
+const list = new DoublyLinkedList();
+
+//
+
+console.log(list);
