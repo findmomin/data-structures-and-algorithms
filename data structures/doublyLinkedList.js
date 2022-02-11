853c29ae16dea65c55357981093d3d9a0b51a83f
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-11 06:15:45 +0600

    Added push to doublyLinkedList

--- data structures/doublyLinkedList.js
+++ data structures/doublyLinkedList.js
@@ -12,10 +12,27 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.length) this.head = this.tail = newNode;
+    else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+
+    return this;
+  }
 }
 
 const list = new DoublyLinkedList();
 
-//
+// Push
+list.push('Hello,');
+list.push('World!');
 
 console.log(list);
