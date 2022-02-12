1919c0d3957d6159ff0d9cf84bd1413b123faf95
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-12 09:27:30 +0600

    Added push to stack

--- data structures/stacks & queues/stack.js
+++ data structures/stacks & queues/stack.js
@@ -1,5 +1,35 @@
-class Stack {
-  constructor() {
-    //
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.size) this.first = this.last = newNode;
+    else {
+      const oldNode = this.first;
+      this.first = newNode;
+      this.first.next = oldNode;
+    }
+
+    return ++this.size;
+  }
+}
+
+const stack = new Stack();
+
+// Push
+console.log(stack.push('Hello'));
+console.log(stack.push('world'));
+
+console.log(stack);
