1c8093dffd2d61cdbbf18274155628a2ce19423f
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-24 08:31:01 +0600

    Added dequeue to priorityQueue

--- data structures/trees/priorityQueue.js
+++ data structures/trees/priorityQueue.js
@@ -28,6 +28,50 @@ class PriorityQueue {
       idx = parentIdx;
     }
   }
+
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+
+    if (!this.values.length) return min;
+
+    this.values[0] = end;
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+
+        if (leftChild.priority < element.priority) swap = leftChildIdx;
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+
+        if (
+          (!swap && rightChild.priority < element.priority) ||
+          (swap && rightChild.priority < leftChild.priority)
+        )
+          swap = rightChildIdx;
+      }
+
+      if (!swap) break;
+
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+
+      idx = swap;
+    }
+
+    return min;
+  }
 }
 
 const priorityQueue = new PriorityQueue();
@@ -39,9 +83,11 @@ console.log(priorityQueue.enqueue(41, 2));
 console.log(priorityQueue.enqueue(41, 3));
 console.log(priorityQueue.enqueue(41, 4));
 
-/* [55, 39, 41, 18, 27, 12, 33] */
+/* [1, 3, 2, 5, 4] */
 
 // Dequeue
+// console.log(priorityQueue.dequeue());
+// console.log(priorityQueue.dequeue());
 // console.log(priorityQueue.dequeue());
 // console.log(priorityQueue.dequeue());
 // console.log(priorityQueue.dequeue());
