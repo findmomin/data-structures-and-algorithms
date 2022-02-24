d63d88188bbae7633a04c0b10069203b3d7b1962
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-25 00:57:02 +0600

    Added values to hashTable

--- data structures/hash tables/hashTable.js
+++ data structures/hash tables/hashTable.js
@@ -35,11 +35,28 @@ class HashTable {
     for (let i = 0; i < this.keyMap.length; i++) {
       if (!this.keyMap[i]) continue;
 
-      for (let j = 0; j < this.keyMap[i].length; j++)
-        keys.push(this.keyMap[i][j][0]);
+      for (let j = 0; j < this.keyMap[i].length; j++) {
+        if (!keys.includes(this.keyMap[i][j][0]))
+          keys.push(this.keyMap[i][j][0]);
+      }
     }
 
     return keys;
+  }
+
+  values() {
+    const values = [];
+
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (!this.keyMap[i]) continue;
+
+      for (let j = 0; j < this.keyMap[i].length; j++) {
+        if (!values.includes(this.keyMap[i][j][1]))
+          values.push(this.keyMap[i][j][1]);
+      }
+    }
+
+    return values;
   }
 }
 
@@ -50,6 +67,8 @@ hashTable.set('cyan', 13);
 hashTable.set('pink', 20);
 hashTable.set('red', 30);
 hashTable.set('magenta', 35);
+hashTable.set('magenta', 35);
+hashTable.set('orangered', 35);
 
 // Get
 // console.log(hashTable.get('cyan'));
