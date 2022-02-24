8b69ed31fa3550f89970e3581fbe28d4bcbbd906
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-25 02:02:54 +0600

    Added addEdge to graph

--- data structures/graph/graph.js
+++ data structures/graph/graph.js
@@ -4,11 +4,25 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1] || !this.adjacencyList[vertex2]) return;
+
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
 }
 
 const graph = new Graph();
 
 // Add vertex
-console.log(graph.addVertex('Ryan'));
+console.log(graph.addVertex('BR'));
+console.log(graph.addVertex('PUBG'));
+console.log(graph.addVertex('Fortnite'));
+
+// Add edge
+console.log(graph.addEdge('BR', 'PUBG'));
+console.log(graph.addEdge('BR', 'Fortnite'));
+console.log(graph.addEdge('BRs', 'Fortnite'));
 
 console.log(graph);
