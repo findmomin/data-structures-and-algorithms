d505e497ae743f86ecb862ba485966bdc46b2678
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-25 01:51:11 +0600

    Added addVertex to graph

--- data structures/graph/graph.js
+++ data structures/graph/graph.js
@@ -1,9 +1,14 @@
 class Graph {
   adjacencyList = {};
 
-  //
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
 }
 
 const graph = new Graph();
+
+// Add vertex
+console.log(graph.addVertex('Ryan'));
 
 console.log(graph);
