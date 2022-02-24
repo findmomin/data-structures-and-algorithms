d127ee53d5918232b2ed0020d45e7b8d0a1daa42
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-24 07:30:04 +0600

    Added extractMax to binaryHeap

--- data structures/trees/binaryHeap.js
+++ data structures/trees/binaryHeap.js
@@ -1,5 +1,5 @@
 class MaxBinaryHeap {
-  values = [41, 39, 33, 18, 27, 12];
+  values = [];
 
   insert(val) {
     this.values.push(val);
@@ -19,14 +19,65 @@ class MaxBinaryHeap {
       idx = parentIdx;
     }
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+
+    if (!this.values.length) return max;
+
+    this.values[0] = end;
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+
+        if (leftChild > element) swap = leftChildIdx;
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+
+        if ((!swap && rightChild > element) || (swap && rightChild > leftChild))
+          swap = rightChildIdx;
+      }
+
+      if (!swap) break;
+
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+
+      idx = swap;
+    }
+
+    return max;
+  }
 }
 
 const heap = new MaxBinaryHeap();
 
 // Insert
+console.log(heap.insert(41));
+console.log(heap.insert(39));
+console.log(heap.insert(33));
+console.log(heap.insert(18));
+console.log(heap.insert(27));
+console.log(heap.insert(12));
 console.log(heap.insert(55));
-console.log(heap.insert(1));
-console.log(heap.insert(45));
-console.log(heap.insert(200));
+
+/*  [55, 39, 41, 18, 27, 12, 33] */
+
+// Remove
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
 
 console.log(heap);
