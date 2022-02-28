36f0e9b20fba9b88b9d225f8098ec50591128e43
Abdul Momin <abdulmomin114477@gmail.com>
2022-02-28 07:29:01 +0600

    Added addEdge to weightedGraph

--- data structures/graphs/weightedGraph.js
+++ data structures/graphs/weightedGraph.js
@@ -4,6 +4,13 @@ class WeightedGraph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+
+  addEdge(vertex1, vertex2, weight) {
+    if (!this.adjacencyList[vertex1] || !this.adjacencyList[vertex2]) return;
+
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
 }
 
 const graph = new WeightedGraph();
@@ -14,4 +21,9 @@ console.log(graph.addVertex('PUBG'));
 console.log(graph.addVertex('Fortnite'));
 console.log(graph.addVertex('Apex'));
 
-console.log(graph);
+// Add edge
+console.log(graph.addEdge('BR', 'PUBG', 2));
+console.log(graph.addEdge('BR', 'Fortnite', 4));
+console.log(graph.addEdge('BR', 'Apex', 6));
+
+console.log(graph.adjacencyList);
