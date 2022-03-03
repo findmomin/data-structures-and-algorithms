b44adf14ce428311edaa55501bdcec3560cd5786
Abdul Momin <abdulmomin114477@gmail.com>
2022-03-03 06:22:05 +0600

    Updated binarySearch

--- searching algorithms/binarySearch.js
+++ searching algorithms/binarySearch.js
@@ -1,8 +1,8 @@
 /* 
-   A function that accepts a sorted array and a value. If the value is in
-   the array, the function returns the index of the value, else
-   returns -1
-   Note: The function has to use binarySearch algorithm 
+  A function that accepts a sorted array and a value. If the value is in
+  the array, the function returns the index of the value, else
+  returns -1
+  Note: The function have to use binarySearch algorithm 
  */
 const binarySearch = (arr = [], val) => {
   let start = 0;
