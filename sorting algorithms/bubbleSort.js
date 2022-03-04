5fd676560027565a9e7c94ce127f71e5af853962
Abdul Momin <abdulmomin114477@gmail.com>
2022-03-04 15:16:59 +0600

    Updated bubbleSort

--- sorting algorithms/bubbleSort.js
+++ sorting algorithms/bubbleSort.js
@@ -1,17 +1,16 @@
 /* 
    A function that accepts an array of integers and returns the 
    sorted array in ascending order. 
- */
+*/
 const bubbleSort = (arr = []) => {
   let noSwaps = false;
 
   for (let i = arr.length; i > 0; i--) {
     noSwaps = true;
 
-    for (let j = 0; j < i - 1; j++) {
-      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      noSwaps = false;
-    }
+    for (let j = 0; j < i - 1; j++)
+      if (arr[j] > arr[j + 1])
+        ([arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]), (noSwaps = false);
 
     if (noSwaps) break;
   }
